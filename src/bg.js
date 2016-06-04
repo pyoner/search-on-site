72ab6d41d89044dba9d34d73f7d1000c4f87835d
Jungle <devex.soft@gmail.com>
2016-06-04 15:00:21 +0500

    code refactoring CSP rule

--- src/bg.js
+++ src/bg.js
@@ -10,7 +10,7 @@ function injectSearchForm(tab) {
 chrome.browserAction.onClicked.addListener(injectSearchForm);
 
 
-var CSP_RULE = "https://pyoner.github.io/";
+var CSP_RULE = "child-src https://pyoner.github.io/";
 var filterUrls = {
     urls: ["http://*/*", "https://*/*"],
     types: ["main_frame"]
@@ -20,12 +20,12 @@ function changeCSPHeader(details){
     for (var i = 0; i < details.responseHeaders.length; i++) {
         var header = details.responseHeaders[i];
         if (header.name == "Content-Security-Policy") {
-            header.value = header.value.replace(/(?:frame|child)\-src\s+(?:'none'|([^;]*));?/gi, "child-src " + CSP_RULE + " $1;");
+            header.value = header.value.replace(/(?:frame|child)\-src\s+(?:'none'|([^;]*));?/gi, CSP_RULE + " $1;");
             if (header.value.indexOf("child-src ") == -1){
                 if(!header.value.test(/;\s*$/)) {
                     header.value += ";";
                 }
-                header.value += "child-src " + CSP_RULE + ";";
+                header.value += CSP_RULE + ";";
             }
             return {responseHeaders: details.responseHeaders}
         }
