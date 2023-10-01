3cab81e3f04e0b7d3234e0f6e6116c13473fccb7
Askar Yusupov <devex.soft@gmail.com>
2023-10-01 21:05:11 +0500

    fix: omnibox works once

--- src/index.js
+++ src/index.js
@@ -1,22 +1,20 @@
 // src/index.ts
-chrome.runtime.onInstalled.addListener(() => {
-  chrome.omnibox.setDefaultSuggestion({ description: "Search On Site" });
-  chrome.omnibox.onInputEntered.addListener(async (text) => {
-    const tabs = await chrome.tabs.query({
-      active: true,
-      currentWindow: true
-    });
-    const tab = tabs[0];
-    if (!tab) {
-      return;
-    }
-    if (!tab.url) {
-      return;
-    }
-    const url = new URL(tab.url);
-    await chrome.search.query({
-      text: `site:${url.hostname} ${text}`,
-      disposition: "CURRENT_TAB"
-    });
+chrome.omnibox.setDefaultSuggestion({ description: "Search On Site" });
+chrome.omnibox.onInputEntered.addListener(async (text) => {
+  const tabs = await chrome.tabs.query({
+    active: true,
+    currentWindow: true
+  });
+  const tab = tabs[0];
+  if (!tab) {
+    return;
+  }
+  if (!tab.url) {
+    return;
+  }
+  const url = new URL(tab.url);
+  await chrome.search.query({
+    text: `site:${url.hostname} ${text}`,
+    disposition: "CURRENT_TAB"
   });
 });
--- src/index.ts
+++ src/index.ts
@@ -1,25 +1,23 @@
-chrome.runtime.onInstalled.addListener(() => {
-  chrome.omnibox.setDefaultSuggestion({ description: "Search On Site" });
-  chrome.omnibox.onInputEntered.addListener(async (text) => {
-    const tabs = await chrome.tabs.query({
-      active: true,
-      currentWindow: true,
-    });
+chrome.omnibox.setDefaultSuggestion({ description: "Search On Site" });
+chrome.omnibox.onInputEntered.addListener(async (text) => {
+  const tabs = await chrome.tabs.query({
+    active: true,
+    currentWindow: true,
+  });
 
-    const tab = tabs[0];
-    if (!tab) {
-      return;
-    }
+  const tab = tabs[0];
+  if (!tab) {
+    return;
+  }
 
-    if (!tab.url) {
-      return;
-    }
+  if (!tab.url) {
+    return;
+  }
 
-    const url = new URL(tab.url);
+  const url = new URL(tab.url);
 
-    await chrome.search.query({
-      text: `site:${url.hostname} ${text}`,
-      disposition: "CURRENT_TAB",
-    });
+  await chrome.search.query({
+    text: `site:${url.hostname} ${text}`,
+    disposition: "CURRENT_TAB",
   });
 });
