2fdc6fb06375093c562d5effd03b893ee38f975a
Askar Yusupov <devex.soft@gmail.com>
2023-10-01 20:37:12 +0500

    fix(index.js, index.ts): add error handling for missing tab or tab URL to prevent potential crashes feat(index.js, index.ts): modify search query to include the current site's hostname in the search text for more accurate results

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,25 @@
 chrome.runtime.onInstalled.addListener(() => {
   chrome.omnibox.setDefaultSuggestion({ description: "Search On Site" });
   chrome.omnibox.onInputEntered.addListener(async (text) => {
-    await chrome.search.query({ text, disposition: "CURRENT_TAB" });
+    const tabs = await chrome.tabs.query({
+      active: true,
+      currentWindow: true,
+    });
+
+    const tab = tabs[0];
+    if (!tab) {
+      return;
+    }
+
+    if (!tab.url) {
+      return;
+    }
+
+    const url = new URL(tab.url);
+
+    await chrome.search.query({
+      text: `site:${url.hostname} ${text}`,
+      disposition: "CURRENT_TAB",
+    });
   });
 });
