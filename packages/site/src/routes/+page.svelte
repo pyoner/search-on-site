d9983a4e606109df15d7e03e15296fa7e416c091
Askar Yusupov <devex.soft@gmail.com>
2023-10-19 13:47:04 +0500

    chore(+page.svelte): refactor search form to use form submission and add focus to input field for better user experience

--- packages/site/src/routes/+page.svelte
+++ packages/site/src/routes/+page.svelte
@@ -1,10 +1,23 @@
 <script lang="ts">
+    
+  function focus (node: HTMLInputElement) {
+    node.focus();
+  }
+
   function search(query: string){
     parent.postMessage({search: {
       query
     }}, "*")
   }
+
+  function handleSearch() {
+    search(query)
+  }
+
   let query = ''
 </script>
 <h1>Search On Site</h1>
-<p><input autofocus type="text" bind:value={query}><button on:click={()=>search(query)}>search</button></p>
+<form on:submit|preventDefault={handleSearch}>
+  <input type="text" use:focus bind:value={query} >
+  <button type="submit" on:click={handleSearch}>search</button>
+</form>
