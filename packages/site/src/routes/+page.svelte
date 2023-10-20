a96e748330ce0961f35e47c566d61f1b01044da3
Askar Yusupov <devex.soft@gmail.com>
2023-10-20 10:16:37 +0500

    style: formatting

--- packages/site/src/routes/+page.svelte
+++ packages/site/src/routes/+page.svelte
@@ -1,26 +1,36 @@
 <script lang="ts">
-    
-  function focus (node: HTMLInputElement) {
-    node.focus();
-  }
+	function focus(node: HTMLInputElement) {
+		node.focus();
+	}
 
-  function search(query: string){
-    parent.postMessage({search: {
-      query
-    }}, "*")
-  }
+	function search(query: string) {
+		parent.postMessage(
+			{
+				search: {
+					query
+				}
+			},
+			'*'
+		);
+	}
 
-  function handleSearch() {
-    search(query)
-  }
+	function handleSearch() {
+		search(query);
+	}
 
-  let query = ''
+	let query = '';
 </script>
 
 <div class="container mx-auto px-4 prose">
-  <h3>Search On Site</h3>
-  <form on:submit|preventDefault={handleSearch}>
-    <input class="input input-bordered w-full max-w-xs" type="text" use:focus bind:value={query} >
-    <button class="btn" type="submit" on:click={handleSearch}>search</button>
-  </form>
+	<h3>Search On Site</h3>
+	<form on:submit|preventDefault={handleSearch} class="join">
+		<input
+			class="input input-bordered w-full max-w-xs join-item"
+			placeholder="Search On Site"
+			type="text"
+			use:focus
+			bind:value={query}
+		/>
+		<button class="btn join-item" type="submit" on:click={handleSearch}>search</button>
+	</form>
 </div>
