b4821bd466e7a5c40f6f91eff3f1aeb90c1dd716
Askar Yusupov <devex.soft@gmail.com>
2023-10-20 11:07:46 +0500

    chore(layout.svelte): add script to set theme based on user's preferred color scheme chore(tailwind.config.js): add 'light' and 'dark' themes to daisyui plugin configuration

--- packages/site/src/routes/+layout.svelte
+++ packages/site/src/routes/+layout.svelte
@@ -1,5 +1,11 @@
-<script>
+<script lang="ts">
+	import { onMount } from 'svelte';
 	import '../app.css';
+
+	onMount(() => {
+		const theme = window.matchMedia('(prefers-color-scheme:dark)')?.matches ? 'dark' : 'ligth';
+		document.firstElementChild?.setAttribute('data-theme', theme);
+	});
 </script>
 
 <slot />
