f739e5b2e1dab819bb1236d7ff76aeb3cbdb4c7a
Askar Yusupov <devex.soft@gmail.com>
2023-10-20 10:37:15 +0500

    refactor(+page.svelte): update search form layout and styles for better user experience

--- packages/site/src/routes/+page.svelte
+++ packages/site/src/routes/+page.svelte
@@ -21,16 +21,22 @@
 	let query = '';
 </script>
 
-<div class="container mx-auto px-4 prose">
-	<h3>Search On Site</h3>
-	<form on:submit|preventDefault={handleSearch} class="join">
-		<input
-			class="input input-bordered w-full max-w-xs join-item"
-			placeholder="Search On Site"
-			type="text"
-			use:focus
-			bind:value={query}
-		/>
-		<button class="btn join-item" type="submit" on:click={handleSearch}>search</button>
-	</form>
+<div class="hero min-h-screen bg-base-200">
+	<div class="hero-content text-center">
+		<div class="max-w-md">
+			<h1 class="text-3xl font-bold">Search On Site</h1>
+			<form on:submit|preventDefault={handleSearch} class="join py-6">
+				<input
+					class="input input-bordered w-full max-w-xs join-item form-control"
+					placeholder="Enter your query"
+					type="text"
+					use:focus
+					bind:value={query}
+				/>
+				<button class="btn btn-primary join-item" type="submit" on:click={handleSearch}
+					>search</button
+				>
+			</form>
+		</div>
+	</div>
 </div>
