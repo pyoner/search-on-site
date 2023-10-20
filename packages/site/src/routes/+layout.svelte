01f9278f3b0f0b991cec24eacec43b6bd1f97d63
Askar Yusupov <devex.soft@gmail.com>
2023-10-20 11:47:52 +0500

    refactor(layout.svelte): extract theme-related logic into separate functions for better readability and maintainability feat(layout.svelte): add support for detecting and setting theme based on prefers-color-scheme media query

--- packages/site/src/routes/+layout.svelte
+++ packages/site/src/routes/+layout.svelte
@@ -2,9 +2,45 @@
 	import { onMount } from 'svelte';
 	import '../app.css';
 
-	onMount(() => {
-		const theme = window.matchMedia('(prefers-color-scheme:dark)')?.matches ? 'dark' : 'ligth';
+	function theme(dark = 'dark', ligth = 'light') {
+		const m = darkThemeMatcher();
+		return {
+			isDark() {
+				return m.matches;
+			},
+			detect() {
+				this.isDark() ? setTheme(dark) : setTheme(ligth);
+			},
+			set(name: string) {
+				setTheme(name);
+			},
+			listen() {
+				const toggle = ({ matches }: MediaQueryListEvent) => {
+					if (matches) {
+						setTheme(dark);
+					} else {
+						setTheme(ligth);
+					}
+				};
+				m.addEventListener('change', toggle);
+
+				return () => m.removeEventListener('change', toggle);
+			}
+		};
+	}
+
+	function darkThemeMatcher() {
+		return window.matchMedia('(prefers-color-scheme:dark)');
+	}
+
+	function setTheme(theme: string) {
 		document.firstElementChild?.setAttribute('data-theme', theme);
+	}
+
+	onMount(() => {
+		const t = theme();
+		t.detect();
+		return t.listen();
 	});
 </script>
 
