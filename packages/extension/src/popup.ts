fae2e4957ea2f5c1fec96f5a6c960cba63b31961
Askar Yusupov <devex.soft@gmail.com>
2023-10-21 11:02:01 +0500

    chore(popup.html): update iframe source URL to https://www.searchon.site/ for the popup window feat(popup.ts): add logic to dynamically set the iframe source URL based on the runtime environment

--- packages/extension/src/popup.ts
+++ packages/extension/src/popup.ts
@@ -1,6 +1,15 @@
 /// <reference lib="dom" />
 import { search } from "./helpers";
 
+const DEV_ID = "kdfeccgoggdgloiblniaimnabknamnca";
+const DEV_URL = "http://localhost:5173/";
+
+if (chrome.runtime.id === DEV_ID) {
+  document.addEventListener("DOMContentLoaded", () => {
+    document.getElementById("son")?.setAttribute("src", DEV_URL);
+  });
+}
+
 window.addEventListener("message", async (event) => {
   const site = new URL(document.URL).searchParams.get("site");
   if (!site) {
