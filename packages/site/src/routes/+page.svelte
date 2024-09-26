e9edc84b0636b545bf9c82368887d968b9f10ffd
Askar Yusupov <devex.soft@gmail.com>
2024-09-26 12:58:11 +0500

    refactor(layout.svelte, page.svelte): remove unused theme management code and simplify layout structure for better readability and maintainability style(page.svelte): update search form structure to use fieldset for better semantics and accessibility

--- packages/site/src/routes/+page.svelte
+++ packages/site/src/routes/+page.svelte
@@ -46,35 +46,16 @@
 	}
 </script>
 
-<div class="hero min-h-screen bg-base-200">
-	<div class="hero-content text-center">
-		<div class="max-w-md">
-			<h1 class="text-3xl font-bold">Search On Site</h1>
-			<form on:submit|preventDefault={handleSearch} class="join py-6">
-				<input
-					class="input w-full max-w-xs join-item form-control"
-					placeholder="Quick search..."
-					type="text"
-					use:focus
-					bind:value={query}
-				/>
-				<button class="btn btn-primary join-item" type="submit"
-					><svg
-						xmlns="http://www.w3.org/2000/svg"
-						fill="none"
-						viewBox="0 0 24 24"
-						stroke-width="1.5"
-						stroke="currentColor"
-						class="w-6 h-6"
-					>
-						<path
-							stroke-linecap="round"
-							stroke-linejoin="round"
-							d="M21 21l-5.197-5.197m0 0A7.5 7.5 0 105.196 5.196a7.5 7.5 0 0010.607 10.607z"
-						/>
-					</svg>
-				</button>
-			</form>
-		</div>
-	</div>
-</div>
+<h1 class="text-3xl font-bold">Search On Site</h1>
+<form on:submit|preventDefault={handleSearch}>
+	<fieldset role="group">
+		<input
+			class="input w-full max-w-xs join-item form-control"
+			placeholder="Quick search..."
+			type="text"
+			use:focus
+			bind:value={query}
+		/>
+		<button type="submit">search</button>
+	</fieldset>
+</form>
