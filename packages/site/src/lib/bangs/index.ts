d690f2092da3a57063be4b6819cc59e28cad6c8d
Askar Yusupov <devex.soft@gmail.com>
2024-09-26 10:54:16 +0500

    refactor: rename bang s to query

--- packages/site/src/lib/bangs/index.ts
+++ packages/site/src/lib/bangs/index.ts
@@ -2,14 +2,14 @@ import bangs from '$lib/bangs/bangs.json';
 
 export const RE_BANG = /^\s*\!(\w+)(?:\s+|$)(.*)/;
 
-export function parseBang(query: string): [bang: string, s: string] | undefined {
-	const m = RE_BANG.exec(query);
+export function parseBang(s: string): [bang: string, query: string] | undefined {
+	const m = RE_BANG.exec(s);
 	if (!m) {
 		return;
 	}
 
-	const [_, bang, s] = m;
-	return [bang, s];
+	const [_, bang, query] = m;
+	return [bang, query];
 }
 
 export function bangItemStartsWith(bang: string) {
@@ -20,6 +20,6 @@ export function fingBangItem(bang: string) {
 	return bangs.find(({ t }) => t === bang);
 }
 
-export function getBangURL(item: (typeof bangs)[number], s: string) {
-	return item.u.replace('{{{s}}}', encodeURIComponent(s));
+export function getBangURL(item: (typeof bangs)[number], query: string) {
+	return item.u.replace('{{{s}}}', encodeURIComponent(query));
 }
